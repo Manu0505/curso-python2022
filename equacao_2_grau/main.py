ead8929a6091d8234e7a53449f28e940608a9c34
Manuelly Almeida <97247264+Manu0505@users.noreply.github.com>
2022-02-09 13:25:45 -0300

    Update main.py

--- equacao_2_grau/main.py
+++ equacao_2_grau/main.py
@@ -7,22 +7,28 @@ formulario = Form()
 formulario.setupUi(janela)
 janela.show()
 #----------------------------------------------------------------------
+
+def raizes(a, b, c):
+    D = (b**2 - 4*a*c)
+    x1 = (-b + D**(1/2)) / (2*a)
+    x2 = (-b - D**(1/2)) / (2*a)
+
+    print('\nValor de x1: {0}'.format(x1))
+    print('Valor de x2: {0}'.format(x2))
+
+#----------------------------------------------------------------------
 def calcular():
     a = float(formulario.txt_a.text())
     b = float(formulario.txt_b.text())
     c = float(formulario.txt_c.text())
     formulario.lbl_equacao.setText(f"{a}x² + {b}x - {c}")
-    delta = math.pow (b, 2) - 4 * a * c
-    if delta >= 0:
-        x1 = (-b + math.sqrt(delta)) / 2 * a
-        x2 = (-b - math.sqrt(delta)) / 2 * a
-        formulario.lbl_delta.setText(f"Δ = {delta}")
-        formulario.lbl_x1.setText(f"x¹ = {x1}")
-        formulario.lbl_x2.setText(f"x² = {x2}")
-    elif delta < 0:
-        formulario.lbl_delta.setText(f"Δ = {delta}")
-        formulario.lbl_x1.setText(f"x¹ = não existem raízes reais")
-        formulario.lbl_x2.setText(f"x² = não existem raízes reais")
+    D = (b**2 - 4*a*c)
+    x1 = (-b + D**(1/2)) / (2*a)
+    x2 = (-b - D**(1/2)) / (2*a)
+    formulario.lbl_delta.setText(f"Δ = {D}")
+    formulario.lbl_x1.setText(f"x¹ = {x1}")
+    formulario.lbl_x2.setText(f"x² = {x2}")
+
         
 formulario.btn_calcular.clicked.connect(calcular)
 app.exec()
