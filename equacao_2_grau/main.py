564ceb817ca1bac59b2262d90681f70f6e1eb67b
Manuelly Almeida <97247264+Manu0505@users.noreply.github.com>
2022-02-17 22:51:49 -0300

    Update main.py

--- equacao_2_grau/main.py
+++ equacao_2_grau/main.py
@@ -6,29 +6,22 @@ janela = Window()
 formulario = Form()
 formulario.setupUi(janela)
 janela.show()
-#----------------------------------------------------------------------
-
-def raizes(a, b, c):
-    D = (b**2 - 4*a*c)
-    x1 = (-b + D**(1/2)) / (2*a)
-    x2 = (-b - D**(1/2)) / (2*a)
-
-    print('\nValor de x1: {0}'.format(x1))
-    print('Valor de x2: {0}'.format(x2))
 
 #----------------------------------------------------------------------
+
 def calcular():
     a = float(formulario.txt_a.text())
     b = float(formulario.txt_b.text())
     c = float(formulario.txt_c.text())
     formulario.lbl_equacao.setText(f"{a}x² + {b}x - {c}")
-    D = (b**2 - 4*a*c)
-    x1 = (-b + D**(1/2)) / (2*a)
-    x2 = (-b - D**(1/2)) / (2*a)
-    formulario.lbl_delta.setText(f"Δ = {D}")
+    delta = (b**2 - 4*a*c)
+    x1 = (-b + delta**(1/2)) / (2*a)
+    x2 = (-b - delta**(1/2)) / (2*a)
+    formulario.lbl_delta.setText(f"Δ = {delta}")
     formulario.lbl_x1.setText(f"x¹ = {x1}")
     formulario.lbl_x2.setText(f"x² = {x2}")
 
-        
+#----------------------------------------------------------------------
+
 formulario.btn_calcular.clicked.connect(calcular)
 app.exec()
